d098fe96f033385933191381c33dc9b135bf5f2b
Mahmoodreza Aarabi <madoodia@gmail.com>
2020-06-29 14:07:25 -0700

    my MObjFirst plugin is developed

---
+++ MyPlugins/MObjFirst/MObjFirst.cpp
@@ -0,0 +1,18 @@
+/* --------------------- */
+/* (C) 2020 madoodia.com */
+/* --------------------- */
+
+#include <OBJ/OBJ_Geometry.h>
+#include <OP/OP_Operator.h>
+#include <OP/OP_OperatorTable.h>
+#include <UT/UT_DSOVersion.h> // Avoiding Version information error
+
+void newObjectOperator(OP_OperatorTable *table)
+{
+  table->addOperator(
+      new OP_Operator("MObjFirst", "MObjFirst",
+                      OBJ_Geometry::myConstructor,
+                      OBJ_Geometry::getTemplateList(OBJ_PARMS_PLAIN),
+                      OBJ_Geometry::theChildTableName,
+                      0, 1, 0));
+}
