45b96c30782b4ec1072bf8c84c200913dee7e6ff
Reza Aarabi <madoodia@gmail.com>
2021-03-11 16:33:18 -0800

    make them compatible with hfs 18.5

--- .buildSystem/win/envVars.bat
+++ .buildSystem/win/envVars.bat
@@ -28,7 +28,7 @@ REM OPENCL_LOCATION
 set OPENCL_LOCATION=%SDKS_LOCATION%/OpenCL
 
 REM MAYA DEVKIT_LOCATION
-set HFS=C:\Program Files\Side Effects Software\Houdini 18.0.348
+set HFS=C:\Program Files\Side Effects Software\Houdini 18.5.499
 REM # Building with Visual Studio 2019 (CL version 14.2) is incompatible with houdini
 REM set MSVCDir=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.24.28314
 REM # So have to build with CL 14.1
--- HDK_Samples_Study/testStandaloneApplication/build.bat
+++ HDK_Samples_Study/testStandaloneApplication/build.bat
@@ -4,16 +4,17 @@ REM # (C) 2020 madoodia.com #
 REM # --------------------- #
 
 REM Run this batch file from explorer or ctrl+alt+h
-set ROOT=%1
-set CURRENT_DIR=%2
+@REM set ROOT=%1
+@REM set CURRENT_DIR=%2
 
-set HFS=C:/Program Files/Side Effects Software/Houdini 18.0.348
-set MSVCDir=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.24.28314
+set HFS=C:/Program Files/Side Effects Software/Houdini 18.5.499
+set MSVCDir=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.16.27023
 set PATH=%PATH%;%HFS%/bin;
 
-call "%VCVARS_LOCATION%/vcvarsall.bat" x64
+set TARGET_VS_VERSION=14.16.27023
+call "%VCVARS_LOCATION%/vcvarsall.bat" x64 -vcvars_ver=%TARGET_VS_VERSION%
 
-cd %ROOT%/%CURRENT_DIR%
+@REM cd %ROOT%/%CURRENT_DIR%
 start /w hcustom -s main.cpp
 start /w main.exe
-call gplay sphere.bgeo
+@REM call gplay sphere.bgeo
